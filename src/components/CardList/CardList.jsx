869e30015e425779b486f15df49c57a8a358489c
Naib Rahman <naib.rahman@gmail.com>
2022-02-14 16:56:28 +0000

    API fetch

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,65 +1,91 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import "./CardList.scss";
 import BeerCard from '../BeerCard/BeerCard';
-import beers from "../../data/beers";
-
 
 const CardList = (props) => {
+
+    const [beers, setBeers] = useState([]);
+
     const {abv, classicRange, acidic} = props.checkBoxState
+
 
     const { searchTerm } = props
 
-    const returnBeer = (beer) => {
-        return <BeerCard  image= {beer.image_url} name= {beer.name}
-        tagline= {beer.tagline}
-        />
+    const getBeers = () => {
+        fetch("https://api.punkapi.com/v2/beers")
+        .then(response => {
+            return response.json()
+        }).then(data => {
+            setBeers(data)
+        })
     }
 
-    const BeerJSX = beers.map((beer) => {
-        return returnBeer(beer)
-    });
+    useEffect(() => {
+        return getBeers()
+    }, [])
 
-    const filteredByNameArray =  beers.filter(beer => {
-            return beer.name.toLowerCase().includes(searchTerm.toLowerCase())
-        })
+    const returnBeer = (beer) => {
+        return <BeerCard  image= {beer.image_url} name= {beer.name}
+        tagline= {beer.tagline} key={beers.indexOf(beer)
+        } />
+    }
 
-    const filterByABV = beers.filter(beer => {
-        return beer.abv > 6
-    })
-  
-    
-    const displayBeersByABV = filterByABV.map(beer => {
-        return returnBeer(beer)
-    })
-
-    const displayBeersBySearchTerm = filteredByNameArray.map(beer => {
-            return  returnBeer(beer)
-        })
-    
-        const filterByClassicRange = beers.filter(beer => {
-            return beer.first_brewed.split("/")[1] < 2010
-        })
-
-        const displayBeersByClassicRange = filterByClassicRange.map(beer => {
+    const BeerJSX = () => {
+        return beers.map((beer) => {
             return returnBeer(beer)
-        })
+        });
+    }
 
-        const filterByAcidic = beers.filter(beer => {
-            return beer.ph < 4
-        })
-
-        const displayBeersByAcidic = filterByAcidic.map(beer => {
-            return returnBeer(beer)
-        })
-
-       
+    const filteredByNameArray = () => {
+        return beers.filter(beer => beer.name.toLowerCase().includes(searchTerm.toLowerCase()))
         
-    
+    }
 
+    const displayBeersBySearchTerm = () => {
+        const beerArr = filteredByNameArray()
+        return beerArr.map(beer => returnBeer(beer))
+    }
+
+    const filterByABV = () => {
+        const beerArr = beers.filter(beer => beer.abv > 6)
+        return beerArr
+    }
+    
+    const displayBeersByABV = () => {
+        const beerArr = filterByABV()
+        return beerArr.map(beer => {
+            return returnBeer(beer)
+        })
+    }
+    
+    const filterByClassicRange = () => {
+        const classicArr = beers.filter(beer => beer.first_brewed.split("/")[1] < 2010)
+        return classicArr
+    }
+
+    const displayBeersByClassicRange = () => {
+        const classicArr = filterByClassicRange()
+        return classicArr.map(beer => {
+            return returnBeer(beer)
+        })
+    }
+
+    const filterByAcidic = () => {
+        const beerArr = beers.filter(beer => beer.ph < 4)
+        return beerArr
+    }
+
+    const displayBeersByAcidic = () => {
+        const beerArr = filterByAcidic()
+        return beerArr.map(beer => {
+            return returnBeer(beer)
+        })
+    }
+
+    
     return (
         <div className='cards'>
-            {acidic ? displayBeersByAcidic : classicRange ? displayBeersByClassicRange :  abv ? displayBeersByABV : searchTerm ? displayBeersBySearchTerm : BeerJSX }
-
+            {beers && (acidic ? displayBeersByAcidic() : classicRange ? displayBeersByClassicRange() :  abv ? displayBeersByABV() : searchTerm ? displayBeersBySearchTerm() : BeerJSX())}
         </div>
     )
 }
